4ec503d5db2db797e39ebd48bf0eb622ae86518b
Sander Sluis <sander@specto.nl>
2025-12-01 11:06:28 +0100

    feat: improve delete confirmation dialog

--- src/dataHooks/useInviteUserConstraintsQuery/useInviteUserConstraintsQuery.ts
+++ src/dataHooks/useInviteUserConstraintsQuery/useInviteUserConstraintsQuery.ts
@@ -1,16 +1,30 @@
 import type { UseQueryResult } from '@tanstack/react-query';
 
 import type { UserInvitationConstraints } from '../../api';
-import { OrganizationApi } from '../../api';
+import {
+  CommandName,
+  OrganizationApi,
+  PermissionType,
+} from '../../api';
 import { QUERY_KEY } from '../../models/query';
 import { QueryUtil } from '../../utils/QueryUtil';
 import { useQueryMemo } from '../../hooks/useQueryMemo';
+import { AuthorizationManager } from '../../classes/managers/AuthorizationManager';
 
 
 export const useInviteUserConstraintsQuery = (): UseQueryResult<UserInvitationConstraints> => {
   return useQueryMemo({
     queryKey: QueryUtil.getGenericKey(QUERY_KEY.INVITE_USER_CONSTRAINTS),
     queryFn: async ({ signal }) => {
+      if (!AuthorizationManager.instance.doesUserHavePermission([
+        { command_name: CommandName.RetrieveInviteUserConstraintsCommand, permission_type: PermissionType.EXECUTE },
+      ])) {
+        return {
+          roles: [],
+          organization_ids: [],
+        };
+      }
+
       const response = await OrganizationApi.getInstance().inviteUserConstraints({ signal });
       return response.data;
     },
---
+++ src/pages/CrudPage/CrudPageDeleteDialog.tsx
@@ -0,0 +1,85 @@
+import type { ReactElement } from 'react';
+import {
+  useCallback,
+  useEffect,
+} from 'react';
+import SaveIcon from '@mui/icons-material/Save';
+import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/material';
+
+import type { DialogAction } from '../../components/ui/Dialog';
+import type {
+  WithDialogRenderProps,
+  WithDialogRefMethods,
+} from '../../hoc/withDialog';
+import { withDialog } from '../../hoc/withDialog';
+import type { GenericData } from '../../models/data';
+import { TestIdUtil } from '../../utils/TestIdUtil';
+
+
+export interface CrudPageDeleteDialogOpenProps<TData extends GenericData> {
+  readonly item: TData;
+}
+export interface CrudPageDeleteDialogProps<TData extends GenericData> extends WithDialogRenderProps<CrudPageDeleteDialogOpenProps<TData>> {
+  readonly onConfirm: (item: TData) => void;
+  readonly getName: (item: TData) => string;
+  readonly createItemDialogTitle?: string;
+}
+export type CrudPageDeleteDialogRefMethods<TData extends GenericData> = WithDialogRefMethods<CrudPageDeleteDialogProps<TData>, CrudPageDeleteDialogOpenProps<TData>>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const CrudPageDeleteDialog = withDialog<CrudPageDeleteDialogProps<any>, CrudPageDeleteDialogOpenProps<any>>(<TData extends GenericData>(
+  {
+    onConfirm,
+    onClose,
+    openProps,
+    onTitleChange,
+    onActionsChange,
+    getName,
+    createItemDialogTitle,
+  }: CrudPageDeleteDialogProps<TData>,
+): ReactElement => {
+  const [t] = useTranslation();
+
+  const onConfirmButtonClick = useCallback(() => {
+    onConfirm(openProps.item);
+    onClose();
+  }, [onClose, onConfirm, openProps.item]);
+
+  useEffect(() => {
+    const actions: DialogAction[] = [];
+    actions.push({
+      ...TestIdUtil.createAttributes('CrudPageDeleteDialog-closeButton'),
+      color: 'primary',
+      autoFocus: true,
+      variant: 'outlined',
+      onClick: onClose,
+      label: t`Cancel`,
+    });
+    actions.push({
+      ...TestIdUtil.createAttributes('CrudPageDeleteDialog-confirmButton'),
+      color: 'secondary',
+      variant: 'contained',
+      onClick: onConfirmButtonClick,
+      label: t`Confirm`,
+      startIcon: <SaveIcon />,
+    });
+
+    onActionsChange(actions);
+  }, [onActionsChange, t, openProps, onConfirmButtonClick, onClose]);
+
+  useEffect(() => {
+    onTitleChange(`Delete item: ${getName(openProps.item)}`);
+  }, [getName, onTitleChange, openProps, t, createItemDialogTitle]);
+
+  return (
+    <Box>
+      {`Are you sure you want to delete the item "${getName(openProps.item)}"?`}
+    </Box>
+  );
+}, {
+  testId: 'CrudPageDeleteDialog',
+  maxWidth: 'lg',
+  fullWidth: true,
+  defaultTitle: '',
+});
