826a9b8faac7a350001cff92129ed815bf9f7a7b
Sander Sluis <sander@specto.nl>
2026-02-20 10:37:20 +0100

    feat: improve table performance

--- src/components/epi/EpiLineList/EpiLineListTitle.tsx
+++ src/components/epi/EpiLineList/EpiLineListTitle.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useStore } from 'zustand';
 import { useShallow } from 'zustand/shallow';
+import { Typography } from '@mui/material';
 
 import { EpiDashboardStoreContext } from '../../../stores/epiDashboardStore';
 
@@ -12,8 +13,16 @@ export const EpiLineListTitle = () => {
   const selectedIds = useStore(epiStore, useShallow((state) => state.selectedIds));
 
   return (
-    <strong>
+    <Typography
+      component={'div'}
+      sx={{
+        fontWeight: 600,
+        lineHeight: theme => theme.spacing(3),
+      }}
+      role={'status'}
+      aria-live={'polite'}
+    >
       {t('Line list: {{shownCount}} cases, {{selectedCount}} selected', { shownCount: sortedData.length ?? 0, selectedCount: selectedIds.length ?? 0 })}
-    </strong>
+    </Typography>
   );
 };
