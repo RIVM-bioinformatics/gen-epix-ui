99e90e2dfc6d6f86486a7abb7440c10ff5f53bbe
Sander Sluis <sander@specto.nl>
2026-02-26 09:36:23 +0100

    fix: fix accessibility of the table header dividers

--- src/components/ui/Table/classNames.ts
+++ src/components/ui/Table/classNames.ts
@@ -1,5 +1,6 @@
 export const tableHeaderCellClassNames = {
   root: 'GENEPIX-TableHeaderCell',
+  content: 'GENEPIX-TableHeaderCell-content',
   columnDivider: 'GENEPIX-TableHeaderCell-columnDivider',
   sortLabelIcon: 'GENEPIX-TableHeaderCell-sortLabelIcon',
   filterLabelIcon: 'GENEPIX-TableHeaderCell-filterLabelIcon',
