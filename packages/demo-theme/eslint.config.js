6840b3d282dadf1948dc999688de0b92285fe8a9
Sander Sluis <sander@specto.nl>
2026-04-13 13:01:10 +0200

    feat: split packages

--- packages/demo-theme/eslint.config.js
+++ packages/demo-theme/eslint.config.js
@@ -1,28 +1,3 @@
 import baseEslintConfig from '@gen-epix/eslint-config';
 
-/** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
-const eslintConfig = baseEslintConfig.map((config) => ({
-	...config,
-	...(config.files ? { files: [...config.files] } : {}),
-	...(config.ignores ? { ignores: [...config.ignores] } : {}),
-	...(config.languageOptions
-		? {
-				languageOptions: {
-					...config.languageOptions,
-					...(config.languageOptions.parserOptions
-						? { parserOptions: { ...config.languageOptions.parserOptions } }
-						: {}),
-				},
-			}
-		: {}),
-}));
-
-/** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
-eslintConfig
-	.filter((config) => config.languageOptions?.parserOptions?.project)
-	.forEach((config) => {
-		config.languageOptions.parserOptions.project = './tsconfig.json';
-		config.languageOptions.parserOptions.tsconfigRootDir = import.meta.dirname;
-	});
-
-export default eslintConfig;
+export default baseEslintConfig;
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,30 +1,5 @@
 import baseEslintConfig from '@gen-epix/eslint-config';
 
-/** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
-const eslintConfig = baseEslintConfig.map((config) => ({
-  ...config,
-  ...(config.files ? { files: [...config.files] } : {}),
-  ...(config.ignores ? { ignores: [...config.ignores] } : {}),
-  ...(config.languageOptions
-    ? {
-        languageOptions: {
-          ...config.languageOptions,
-          ...(config.languageOptions.parserOptions
-            ? { parserOptions: { ...config.languageOptions.parserOptions } }
-            : {}),
-        },
-      }
-    : {}),
-}));
-
-/** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
-eslintConfig
-  .filter((config) => config.languageOptions?.parserOptions?.project)
-  .forEach((config) => {
-    config.languageOptions.parserOptions.project = './tsconfig.json';
-    config.languageOptions.parserOptions.tsconfigRootDir = import.meta.dirname;
-  });
-
 // Example of adding a custom rule
 // /** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
 // const rules = eslintConfig.find(c => c.files && c.files.includes('**/*.ts')).rules;
@@ -38,4 +13,4 @@ eslintConfig
 // }];
 
 
-export default eslintConfig;
+export default baseEslintConfig;
