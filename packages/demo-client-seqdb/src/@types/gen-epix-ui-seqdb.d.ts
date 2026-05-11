7d5faeae60b1f0553e788f738266b415d2383073
Sander Sluis <sander@specto.nl>
2026-05-11 13:55:48 +0200

    feat: split frontends

---
+++ packages/demo-client-seqdb/src/@types/gen-epix-ui-seqdb.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="@gen-epix/ui-seqdb/client" />
+/// <reference types="@gen-epix/ui-seqdb/src/@types/ui-seqdb" />
--- packages/ui-casedb/src/@types/ui.d.ts
+++ packages/ui-casedb/src/@types/ui.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="@gen-epix/ui/client" />
 /// <reference types="@gen-epix/ui/src/@types/ui" />
+/// <reference types="./ui-casedb" />
